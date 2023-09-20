5581df3418b0959774caa8cf7d30c0b606ca19d1
samad <abdessamad.bouchelga@gmail.com>
2023-09-19 17:26:39 -0700

    update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models import storage_type
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents Amenity Class"""
+    __tablename__ = 'amenities'
+
+    if storage_type != 'db':
+        name = ""
+    else:
+        name = Column(String(128), nullable=False)
